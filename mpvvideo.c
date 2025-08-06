fbc766626664c26a50b67000fa72e3c0f87eb2f6
petko02 <160341742+petko02@users.noreply.github.com>
2025-08-06 13:06:47 +0100

    mpvvideo.c

--- mpvvideo.c
+++ mpvvideo.c
@@ -1,47 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <gtk/gtk.h>
 #include <mpv/client.h>
 #include "wlxplugin.h"
 
 static mpv_handle *mpv = NULL;
-static GtkWidget *window = NULL;
 
-HWND DCPCALL ListLoad(HWND ParentWin, char *FileToLoad, int ShowFlags) {
-    gtk_init(0, NULL);
-
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "MpvViewer");
-    gtk_widget_set_size_request(window, 800, 600);
-    gtk_widget_realize(window);
-    gtk_widget_show_all(window);
-
+HWND DCPCALL ListLoad(HWND ParentWin, char *FileToLoad, int ShowFlags)
+{
     mpv = mpv_create();
     if (!mpv) return 0;
 
-    mpv_initialize(mpv);
+    // Embed video into the host window provided by Double Commander
+    char wid[64];
+    snprintf(wid, sizeof(wid), "%p", (void *)ParentWin);
+    mpv_set_option_string(mpv, "wid", wid);
 
-    const char *cmd[] = { "loadfile", FileToLoad, NULL };
+    if (mpv_initialize(mpv) < 0) {
+        mpv_terminate_destroy(mpv);
+        mpv = NULL;
+        return 0;
+    }
+
+    const char *cmd[] = {"loadfile", FileToLoad, NULL};
     mpv_command(mpv, cmd);
 
-    return (HWND)window;
+    return ParentWin;
 }
 
-void DCPCALL ListCloseWindow(HWND ListWin) {
+void DCPCALL ListCloseWindow(HWND ListWin)
+{
     if (mpv) {
         mpv_terminate_destroy(mpv);
         mpv = NULL;
     }
-    if (window) {
-        gtk_widget_destroy(window);
-        window = NULL;
-    }
 }
 
-void DCPCALL ListGetDetectString(char *DetectString, int maxlen) {
-    strncpy(DetectString, 
-        "EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"AVI\"|EXT=\"WMV\"|EXT=\"MOV\"|EXT=\"WEBM\"|"
-        "EXT=\"FLV\"|EXT=\"M4V\"|EXT=\"MPG\"|EXT=\"MPEG\"|EXT=\"3GP\"|EXT=\"WAV\"", 
-        maxlen - 1);
+void DCPCALL ListGetDetectString(char *DetectString, int maxlen)
+{
+    snprintf(DetectString, maxlen - 1,
+        "EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"AVI\"|EXT=\"WMV\"|EXT=\"MOV\"|"
+        "EXT=\"WEBM\"|EXT=\"FLV\"|EXT=\"M4V\"|EXT=\"MPG\"|EXT=\"MPEG\"|"
+        "EXT=\"3GP\"|EXT=\"WAV\"");
 }
