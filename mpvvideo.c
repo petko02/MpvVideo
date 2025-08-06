268ad222287dee1e7ae6a3e8181faa2305d1f47e
petko02 <160341742+petko02@users.noreply.github.com>
2025-08-06 17:57:31 +0100

    mpvvideo.c

--- mpvvideo.c
+++ mpvvideo.c
@@ -7,43 +7,42 @@ static mpv_handle *mpv = NULL;
 static mpv_opengl_cb_context *mpv_gl = NULL;
 static NSView *videoView = nil;
 
-intptr_t DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
-{
-    NSView *parent = (__bridge NSView*)ParentWin;
+HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags) {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-    videoView = [[NSView alloc] initWithFrame:parent.bounds];
+    NSView *parentView = (NSView *)ParentWin;
+    videoView = [[NSView alloc] initWithFrame:[parentView bounds]];
     [videoView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-    [parent addSubview:videoView];
+    [parentView addSubview:videoView];
 
     mpv = mpv_create();
-    if (!mpv) return 0;
+    if (!mpv) return (HWND)videoView;
 
+    // Set video output
+    mpv_set_option_string(mpv, "vo", "libmpv");
     mpv_initialize(mpv);
 
-    mpv_set_option_string(mpv, "vo", "libmpv");
+    // Get OpenGL context
     mpv_gl = mpv_get_sub_api(mpv, MPV_SUB_API_OPENGL_CB);
+    if (!mpv_gl) return (HWND)videoView;
 
-    NSOpenGLPixelFormatAttribute attrs[] = {
-        NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
-        NSOpenGLPFAColorSize, 24,
-        NSOpenGLPFADoubleBuffer,
-        0
-    };
-    NSOpenGLPixelFormat *pf = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
-    NSOpenGLContext *glContext = [[NSOpenGLContext alloc] initWithFormat:pf shareContext:nil];
-    [glContext setView:videoView];
-    [glContext makeCurrentContext];
+    // Use the NSView layer for rendering
+    NSOpenGLContext *glContext = [[NSOpenGLContext alloc] initWithFormat:[NSOpenGLPixelFormat alloc]
+                                                             shareContext:nil];
+    [videoView setWantsLayer:YES];
+    [videoView.layer setContentsScale:[[NSScreen mainScreen] backingScaleFactor]];
 
+    // Attach mpv rendering to the view
     mpv_opengl_cb_init_gl(mpv_gl, NULL, NULL, NULL);
 
     const char *cmd[] = {"loadfile", FileToLoad, NULL};
-    mpv_command_async(mpv, 0, cmd);
+    mpv_command(mpv, cmd);
 
-    return (intptr_t)videoView;
+    [pool drain];
+    return (HWND)videoView;
 }
 
-void DCPCALL ListCloseWindow(HWND ListWin)
-{
+void DCPCALL ListCloseWindow(HWND ListWin) {
     if (mpv_gl) {
         mpv_opengl_cb_uninit_gl(mpv_gl);
         mpv_gl = NULL;
@@ -54,12 +53,17 @@ void DCPCALL ListCloseWindow(HWND ListWin)
     }
     if (videoView) {
         [videoView removeFromSuperview];
+        [videoView release];
         videoView = nil;
     }
 }
 
-void DCPCALL ListGetDetectString(char* DetectString, int maxlen)
-{
-    snprintf(DetectString, maxlen,
-             "EXT=\"AVI\"|EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"MOV\"|EXT=\"WMV\"|EXT=\"FLV\"|EXT=\"WEBM\"|EXT=\"M4V\"|EXT=\"MPEG\"|EXT=\"MPG\"");
+int DCPCALL ListGetDetectString(char* DetectString, int maxlen) {
+    const char *detect =
+        "EXT=\"MP4\" | EXT=\"M4V\" | EXT=\"MKV\" | EXT=\"AVI\" | EXT=\"MOV\" | "
+        "EXT=\"WMV\" | EXT=\"FLV\" | EXT=\"WEBM\" | EXT=\"MPG\" | EXT=\"MPEG\" | "
+        "EXT=\"3GP\" | EXT=\"TS\" | EXT=\"OGV\" | EXT=\"VOB\" | EXT=\"ASF\"";
+    strncpy(DetectString, detect, maxlen - 1);
+    DetectString[maxlen - 1] = '\0';
+    return 0;
 }
