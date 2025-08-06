5d429ce1339ec1e543a2c5609379b73c02a5c1c6
petko02 <160341742+petko02@users.noreply.github.com>
2025-08-06 12:43:13 +0100

    mpvvideo.c

--- mpvvideo.c
+++ mpvvideo.c
@@ -7,26 +7,20 @@
 
 static mpv_handle *mpv = NULL;
 static GtkWidget *window = NULL;
-static GtkWidget *widget = NULL;
 
 HWND DCPCALL ListLoad(HWND ParentWin, char *FileToLoad, int ShowFlags) {
     gtk_init(0, NULL);
 
-    // Create a GTK window to embed in DC
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "MpvViewer");
     gtk_widget_set_size_request(window, 800, 600);
     gtk_widget_realize(window);
     gtk_widget_show_all(window);
 
-    // Init MPV
     mpv = mpv_create();
     if (!mpv) return 0;
 
-    mpv_set_option_string(mpv, "vo", "libmpv");
     mpv_initialize(mpv);
-
-    mpv_set_option_string(mpv, "wid", g_strdup_printf("%lu", (unsigned long)GDK_WINDOW_XID(gtk_widget_get_window(window))));
 
     const char *cmd[] = { "loadfile", FileToLoad, NULL };
     mpv_command(mpv, cmd);
@@ -45,7 +39,9 @@ void DCPCALL ListCloseWindow(HWND ListWin) {
     }
 }
 
-int DCPCALL ListGetDetectString(char *DetectString, int maxlen) {
-    strncpy(DetectString, "EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"AVI\"|EXT=\"WMV\"|EXT=\"MOV\"|EXT=\"WEBM\"|EXT=\"FLV\"|EXT=\"M4V\"|EXT=\"MPG\"|EXT=\"MPEG\"|EXT=\"3GP\"", maxlen - 1);
-    return TRUE;
+void DCPCALL ListGetDetectString(char *DetectString, int maxlen) {
+    strncpy(DetectString, 
+        "EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"AVI\"|EXT=\"WMV\"|EXT=\"MOV\"|EXT=\"WEBM\"|"
+        "EXT=\"FLV\"|EXT=\"M4V\"|EXT=\"MPG\"|EXT=\"MPEG\"|EXT=\"3GP\"|EXT=\"WAV\"", 
+        maxlen - 1);
 }
