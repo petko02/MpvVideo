8044b2b66cf89081d796fbf3e5a9a94a5e21241c
petko02 <160341742+petko02@users.noreply.github.com>
2025-08-07 18:23:10 +0100

    Update mpvvideo.mm

--- mpvvideo.mm
+++ mpvvideo.mm
@@ -1,39 +1,33 @@
 // mpvvideo.mm
 #import <Cocoa/Cocoa.h>
-#import <AppKit/AppKit.h>
-#import <QuickLook/QuickLook.h>
-#import <Foundation/Foundation.h>
 #import "wlxplugin.h"
 
 NSView *previewView = nil;
 
-// Required entry point: load the plugin window
-int DCPCALL ListLoadPluginW(HWND ParentWin, WCHAR* FileToLoad, int ShowFlags) {
+void* DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags) {
     @autoreleasepool {
-        if (!FileToLoad) return LISTPLUGIN_ERROR;
+        NSString *path = [NSString stringWithUTF8String:FileToLoad];
+        NSURL *url = [NSURL fileURLWithPath:path];
+        NSView *parent = (__bridge NSView *)(ParentWin);
 
-        NSView *parent = (__bridge NSView *)ParentWin;
+        NSTextView *textView = [[NSTextView alloc] initWithFrame:parent.bounds];
+        textView.string = [NSString stringWithFormat:@"Preview: %@", [url lastPathComponent]];
+        textView.editable = NO;
+        textView.drawsBackground = NO;
+        textView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
 
-        NSURL *fileURL = [NSURL fileURLWithPath:[NSString stringWithCharacters:FileToLoad length:wcslen(FileToLoad)]];
-        QLPreviewView *qlView = [[QLPreviewView alloc] initWithFrame:parent.bounds style:QLPreviewViewStyleNormal];
-        qlView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-        [qlView setPreviewItem:(id<QLPreviewItem>)fileURL];
-        [parent addSubview:qlView];
-        previewView = qlView;
+        [parent addSubview:textView];
+        previewView = textView;
 
-        return (int)(__bridge_retained void *)qlView;
+        return (__bridge_retained void *)textView;
     }
 }
 
-// Required entry point: unload the plugin
-void DCPCALL ListCloseWindowW(HWND ListWin) {
-    @autoreleasepool {
-        NSView *view = (__bridge_transfer NSView *)ListWin;
-        [view removeFromSuperview];
-    }
+void DCPCALL ListCloseWindow(void* ListWin) {
+    NSView *view = (__bridge_transfer NSView *)ListWin;
+    [view removeFromSuperview];
 }
 
-// Optional: return supported extensions
-void DCPCALL ListGetDetectStringW(WCHAR* DetectString, int maxlen) {
-    swprintf(DetectString, maxlen, L"EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"AVI\"|EXT=\"MOV\"|EXT=\"WMV\"");
+void DCPCALL ListGetDetectString(char* DetectString, int maxlen) {
+    snprintf(DetectString, maxlen, "EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"AVI\"|EXT=\"MOV\"|EXT=\"WMV\"");
 }
