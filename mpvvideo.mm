cc16e14e586ad76e09130ab55d22fa403472272b
petko02 <160341742+petko02@users.noreply.github.com>
2025-08-07 18:16:55 +0100

    Update mpvvideo.mm

--- mpvvideo.mm
+++ mpvvideo.mm
@@ -1,65 +1,39 @@
+// mpvvideo.mm
 #import <Cocoa/Cocoa.h>
-#import <QuartzCore/QuartzCore.h>
-#import <OpenGL/gl3.h>
-#import <dlfcn.h>
-#include <string.h>
+#import <AppKit/AppKit.h>
+#import <QuickLook/QuickLook.h>
+#import <Foundation/Foundation.h>
+#import "wlxplugin.h"
 
-#define LISTPLUGIN_OK 0
-#define LISTPLUGIN_ERROR 1
+NSView *previewView = nil;
 
-typedef void* (__cdecl *mpv_client_api_func)(const char *name);
+// Required entry point: load the plugin window
+int DCPCALL ListLoadPluginW(HWND ParentWin, WCHAR* FileToLoad, int ShowFlags) {
+    @autoreleasepool {
+        if (!FileToLoad) return LISTPLUGIN_ERROR;
 
-typedef struct mpv_handle mpv_handle;
-typedef struct mpv_render_context mpv_render_context;
+        NSView *parent = (__bridge NSView *)ParentWin;
 
-typedef int mpv_opengl_cb_get_proc_address_fn(void *ctx, const char *name);
-typedef struct mpv_opengl_cb_context mpv_opengl_cb_context;
+        NSURL *fileURL = [NSURL fileURLWithPath:[NSString stringWithCharacters:FileToLoad length:wcslen(FileToLoad)]];
+        QLPreviewView *qlView = [[QLPreviewView alloc] initWithFrame:parent.bounds style:QLPreviewViewStyleNormal];
+        qlView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+        [qlView setPreviewItem:(id<QLPreviewItem>)fileURL];
+        [parent addSubview:qlView];
+        previewView = qlView;
 
-typedef mpv_render_context* (*mpv_get_sub_api_fn)(void *);
-typedef int (*mpv_render_context_set_update_callback_fn)(mpv_render_context*, void(*cb)(void*), void*);
-typedef int (*mpv_render_context_update_fn)(mpv_render_context*);
-typedef int (*mpv_render_context_render_fn)(mpv_render_context*, void*);
-
-mpv_get_sub_api_fn mpv_get_sub_api;
-mpv_render_context_set_update_callback_fn mpv_render_context_set_update_callback;
-mpv_render_context_update_fn mpv_render_context_update;
-mpv_render_context_render_fn mpv_render_context_render;
-
-@interface MPVView : NSOpenGLView {
-  mpv_render_context *mpv_ctx;
-}
-@end
-
-@implementation MPVView
-
-- (void)drawRect:(NSRect)dirtyRect {
-  if (mpv_ctx && mpv_render_context_render) {
-    mpv_render_context_render(mpv_ctx, NULL);
-  }
+        return (int)(__bridge_retained void *)qlView;
+    }
 }
 
-@end
-
-extern "C" int ListLoadW(const wchar_t* fileToLoad, int showFlags) {
-  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
-  NSRect frame = NSMakeRect(0, 0, 800, 600);
-  NSWindow *window = [[NSWindow alloc] initWithContentRect:frame
-                                                 styleMask:(NSWindowStyleMaskTitled |
-                                                            NSWindowStyleMaskClosable |
-                                                            NSWindowStyleMaskResizable)
-                                                   backing:NSBackingStoreBuffered
-                                                     defer:NO];
-  [window setTitle:@"MpvVideo.wlx"];
-  [window makeKeyAndOrderFront:nil];
-
-  MPVView *view = [[MPVView alloc] initWithFrame:frame];
-  [window setContentView:view];
-
-  [pool drain];
-  return LISTPLUGIN_OK;
+// Required entry point: unload the plugin
+void DCPCALL ListCloseWindowW(HWND ListWin) {
+    @autoreleasepool {
+        NSView *view = (__bridge_transfer NSView *)ListWin;
+        [view removeFromSuperview];
+    }
 }
 
-extern "C" void ListGetDetectString(char* DetectString, int maxlen) {
-  snprintf(DetectString, maxlen, "EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"AVI\"|EXT=\"MOV\"|EXT=\"WMV\"");
+// Optional: return supported extensions
+void DCPCALL ListGetDetectStringW(WCHAR* DetectString, int maxlen) {
+    swprintf(DetectString, maxlen, L"EXT=\"MP4\"|EXT=\"MKV\"|EXT=\"AVI\"|EXT=\"MOV\"|EXT=\"WMV\"");
 }
